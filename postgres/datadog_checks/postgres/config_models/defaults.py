69d228738f8e12f2bf33f7a8694d687fca80a8b5
jmeunier28 <jojo.meunier@datadoghq.com>
2021-08-08 16:22:26 -0700

    update postgres data models

--- postgres/datadog_checks/postgres/config_models/defaults.py
+++ postgres/datadog_checks/postgres/config_models/defaults.py
@@ -88,6 +88,10 @@ def instance_port(field, value):
     return 5432
 
 
+def instance_query_activity(field, value):
+    return get_default_field_value(field, value)
+
+
 def instance_query_metrics(field, value):
     return get_default_field_value(field, value)
 
